9aa76eee5f53b13fbf5eaa98e9520ec844c5f5fd
unknown <sean@sdmworld.co.uk>
2013-12-10 11:29:20 +0000

    Fix EBADF on Windows

--- tasks/grunt-latte.js
+++ tasks/grunt-latte.js
@@ -5,10 +5,12 @@ module.exports = function(grunt) {
 
 	grunt.registerMultiTask('latte', function () {
 		var done = this.async();
-		compiler.compileDir(this.data.inputDir, this.data.outputDir, this.data.options || {}).on('end', function () {
+		compiler.compileDir(this.data.inputDir, this.data.outputDir, this.data.options || {})
+		.on('end', function () {
 			done();
-		}).on('error', function (e) {
-			console.error(e);
+		})
+		.on('error', function (e) {
+			console.error('*** error:', e);
 			done(false);
 		})
 	});
