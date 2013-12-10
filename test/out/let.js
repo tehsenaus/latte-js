0d54fcd443ed33ca2d5cf1875a161564a20944fd
unknown <sean@sdmworld.co.uk>
2013-12-10 12:12:41 +0000

    Fixes #4

--- test/out/let.js
+++ test/out/let.js
@@ -1,3 +1,35 @@
+if (typeof StopIteration === 'undefined') {
+    var StopIteration = 'StopIteration';
+}
+function __iterator(v) {
+    if (typeof v.iterator === 'function') {
+        return v.iterator();
+    }
+    if (typeof v === 'string' || Object.prototype.toString.call(v) === '[object Array]') {
+        var a = v, i = 0;
+        return {
+            next: function () {
+                if (i < a.length) {
+                    return a[i++];
+                }
+                throw StopIteration;
+            }
+        };
+    }
+    var o = v, a = Object.keys(o), i = 0;
+    return {
+        next: function () {
+            if (i < a.length) {
+                var k = a[i++];
+                return [
+                    k,
+                    o[k]
+                ];
+            }
+            throw StopIteration;
+        }
+    };
+}
 var should = require('should');
 describe('Let Statement', function () {
     it('defines variables in block scope', function (done) {
@@ -32,5 +64,48 @@ describe('Let Statement', function () {
             r.should.equal(30);
             done();
         });
+    });
+    it('works with for (of)', function (done) {
+        var a = [
+                10,
+                20
+            ], r = 0;
+        var __t0 = __iterator(a);
+        while (true) {
+            try {
+                throw null;
+            } catch (x) {
+                try {
+                    x = __t0.next();
+                } catch (e) {
+                    if (e === StopIteration)
+                        break;
+                    throw e;
+                }
+                process.nextTick(function () {
+                    r += x;
+                });
+            }
+        }
+        process.nextTick(function () {
+            r.should.equal(30);
+            done();
+        });
+    });
+    it('works with return statement', function () {
+        var a = [
+                10,
+                20
+            ];
+        function f() {
+            for (var __t1 in a) {
+                try {
+                    throw __t1;
+                } catch (i) {
+                    return a[i];
+                }
+            }
+        }
+        f().should.equal(10);
     });
 });
